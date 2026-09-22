13825cf16aa852b16ae1141a408f6843d7d5758a
Poul1337 <paweljarujaruszewski@gmail.com>
2026-09-22 16:56:09 +0200

    feat: add two tests

--- packages/contracts/test/Crowdfunding.t.sol
+++ packages/contracts/test/Crowdfunding.t.sol
@@ -13,13 +13,34 @@ contract CrowdfundingTest is Test {
     }
 
 
-    function testCampainCountIncrement() public {
-        crowdfunding.createCampaign(1 ether, block.timestamp + 30 days);
+    function testCreateCampaign() public {
+        uint256 expectedDeadline = block.timestamp + 30 days;
+        crowdfunding.createCampaign(1 ether, expectedDeadline);
+
+        (uint256 goal, address owner, uint256 raised, uint256 deadline, bool claimed) = crowdfunding.campaigns(1);
 
         assertEq(crowdfunding.campaignCount(), 1);
+        assertEq(goal, 1 ether);
+        assertEq(owner, address(this));
+        assertEq(raised, 0);
+        assertEq(deadline, expectedDeadline);
+        assertEq(claimed, false);
+    }
 
-        crowdfunding.createCampaign(2 ether, block.timestamp + 60 days);
+    function testRevertWhenGoalTooSmall() public {
+        vm.expectRevert("Goal is too small to start campaign");
 
-        assertEq(crowdfunding.campaignCount(), 2);
+        crowdfunding.createCampaign(0 ether, block.timestamp + 30 days);
+
+        assertEq(crowdfunding.campaignCount(), 0);
+    }
+
+    function testRevertWhenDeadlineIsInThePast() public {
+        vm.warp(30 days);
+        vm.expectRevert("deadline must be in a future");
+
+        crowdfunding.createCampaign(1 ether, block.timestamp - 1 days);
+
+        assertEq(crowdfunding.campaignCount(), 0);
     }
 }
